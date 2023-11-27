4b0037f7fc2e08848e9d3408a3b42d1ecabea621
valentine-mario <valentine13400@gmail.com>
2023-11-27 12:23:01 +0100

    update

--- app.ex
+++ app.ex
@@ -3,7 +3,21 @@
 # [1, 3, :abc]
 # "hi"
 # 'hello\u348909\n'
-"hello\"\n\n\u123467"
-label = "hello"
-label
-'hj\'\\th'
+# "hello\"\n\n\u123467"
+# label = "hello"
+# label
+# 'hj\'\\th'
+
+k="""
+hello
+this is
+a multi
+line string
+90
+"""
+
+# '''
+# this is another multiline
+# \'
+# string
+# '''
