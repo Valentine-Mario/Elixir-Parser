bda06a7a79d6a84f5c952fb70152a8cc4e659f46
valentine-mario <valentine13400@gmail.com>
2023-11-29 15:33:18 +0100

    added reservekey word

--- app.ex
+++ app.ex
@@ -4,18 +4,19 @@
 # "hi"
 # 'hello\u348909\n'
 # "hello\"\n\n\u123467"
-# label = "hello"
+label = "hello"
 # label
 # 'hj\'\\th'
 
 # k? =nil
 # # b=[[a, 2, 3], []]
-{:abc, nil}
-%{k => 33, {}: 40, a: 10, bb: 1}
-%{}
-%{abc | k => 33, "ab"=>10}
+# {:abc, nil}
+# %{k => 33, {}: 40, a: 10, bb: 1}
+# %{}
+# %{abc | k => 33, "ab"=>10}
 
-%{"bc"=>12, b: 40, c: 400}
-defmodule  do
+# %{"bc"=>12, b: 40, c: 400}
+# defmodule  do
 
-end
+ender =100
+:and = 10
