c5ef045211ac4dfa8e6c1537fcbe76a25ccb58d0
Valentine-mario <valentine13400@gmail.com>
2024-02-02 05:55:41 +0100

    added map parsing

--- src/lib.rs
+++ src/lib.rs
@@ -32,10 +32,31 @@ pub enum Verb {
     Divide,
     Concat,
     ConcatString,
+    And,
+    Or,
+    Merge,
 }
 
 fn flatten(nested: Vec<Vec<Ast>>) -> Vec<Ast> {
     nested.into_iter().flatten().collect()
+}
+
+fn parse_verb(pair: Pair<Rule>) -> Verb {
+    match pair.as_str() {
+        "+" => Verb::Plus,
+        "*" => Verb::Times,
+        "-" => Verb::Minus,
+        "<" => Verb::LessThan,
+        "=" => Verb::Equal,
+        ">" => Verb::LargerThan,
+        "%" => Verb::Divide,
+        "++" => Verb::Concat,
+        "<>" => Verb::ConcatString,
+        "&&" => Verb::And,
+        "||" => Verb::Or,
+        "|" => Verb::Merge,
+        _ => panic!("Unexpected  verb: {}", pair.as_str()),
+    }
 }
 
 pub fn parse(source: &str) -> Result<Vec<Ast>, Box<Error<Rule>>> {
@@ -51,7 +72,7 @@ pub fn parse(source: &str) -> Result<Vec<Ast>, Box<Error<Rule>>> {
 
 pub fn build_ast_from_stat(pair: Pair<Rule>) -> Vec<Ast> {
     let mut ast_return = vec![];
-    for item in pair.clone().into_inner() {
+    for item in pair.into_inner() {
         match item.as_rule() {
             Rule::types => ast_return.push(parse_types(item.into_inner().next().unwrap())),
             Rule::expr => {}
@@ -82,6 +103,58 @@ pub fn parse_types(pair: Pair<Rule>) -> Ast {
                 .map(|x| parse_types(x.into_inner().next().unwrap()))
                 .collect(),
         ),
+        Rule::arrow_first_map => {
+            let mut merge_map: Option<(Box<Ast>, Verb)> = None;
+            let mut map_items: Vec<(&str, Ast)> = vec![];
+            let mut inner_rules = pair.into_inner();
+            if inner_rules.len() > 3 {
+                let mut a = inner_rules.clone();
+                if a.next().unwrap().as_rule() == Rule::ident
+                    && a.next().unwrap().as_rule() == Rule::verbs
+                {
+                    let ident = parse_types(inner_rules.next().unwrap());
+                    let verb = parse_verb(inner_rules.next().unwrap());
+                    merge_map = Some((Box::new(ident), verb))
+                }
+            }
+
+            for i in inner_rules {
+                match i.as_rule() {
+                    Rule::arrow_map_item => {
+                        let mut rule = i.into_inner();
+                        let key = rule.next().unwrap().as_str();
+                        let value = parse_types(rule.next().unwrap().into_inner().next().unwrap());
+                        map_items.push((key, value))
+                    }
+                    Rule::map_arrangment => {
+                        let rule = i.into_inner();
+                        for k in rule {
+                            let key = k.clone().into_inner().next().unwrap().as_str();
+                            let value: Ast = parse_types(
+                                k.into_inner()
+                                    .next_back()
+                                    .unwrap()
+                                    .into_inner()
+                                    .next()
+                                    .unwrap(),
+                            );
+                            map_items.push((key, value))
+                        }
+                    }
+                    Rule::colon_map_item => {
+                        let mut rule = i.into_inner();
+                        let key = rule.next().unwrap().as_str();
+                        let value =
+                            parse_types(rule.next_back().unwrap().into_inner().next().unwrap());
+
+                        map_items.push((key, value))
+                    }
+                    _ => {}
+                }
+            }
+            Ast::Map(merge_map, map_items)
+        }
+        Rule::colon_first_map => Ast::Map(None, vec![]),
         _ => Ast::NoOp,
     }
 }
